659d4673ec56104f992f4dce96a90af2b0e5a95b
Jack <ajackofsometrades@gmail.com>
2018-08-02 16:15:50 -0700

    hello world from index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
-(function (module) {
+(function(module) {
 
-        
+    let root = document.getElementById('root');
+
+    let element = document.createElement('h2');
+
+    element.innerHTML = 'hello worldo';
+
+    root.appendChild(element);
 
 })(window.module = window.module || {});
