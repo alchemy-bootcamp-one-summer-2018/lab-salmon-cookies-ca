727289e053982f91e136c0c036fb0339bebadcbf
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-08-02 15:17:35 -0700

    intital commit

---
+++ js/componets/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function(module) {
+    let html = module.html;
+
+    let stores = [{
+        name: 'Pike Place Market',
+        minCust: 23,
+        maxCust: 65,
+        avgCookies: 6
+    }, {
+        name: 'SeaTac Airport',
+        minCust: 3,
+        maxCust: 24,
+        avgCookies: 1
+    }, {
+        name: 'Seattle Center',
+        minCust: 11,
+        maxCust: 38,
+        avgCookies: 4
+    }, {
+        name: 'Capitol Hill',
+        minCust: 20,
+        maxCust: 38,
+        avgCookies: 2
+    }, {
+        name: 'Alki',
+        minCust: 2,
+        maxCust: 16,
+        avgCookies: 5
+    }];
+
+    let template = function() {
+        return html`
+        <header>
+            <h1>Pat's Salmon Cookie Co.</h1>
+        </header>
+        `;
+    };
+    
+    class App {
+        render() {
+            let dom = template();
+            
+            return dom;
+        }
+    }
+
+    module.App = App
+
+})(window.module = window.module || {});
---
+++ js/componets/store-row.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function(module) {
+    let html = module.html;
+    let App = module.App;
+
+    let template = function(store)
+})
