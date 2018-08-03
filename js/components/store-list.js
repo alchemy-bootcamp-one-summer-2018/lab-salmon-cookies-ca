b103df3b48d9e6650c4e4cbb5ee0c2df1c1905a9
Claire Flanagan <claireflanagan@gmail.com>
2018-08-02 18:11:41 -0700

    working on store.api.js

--- js/components/store-list.js
+++ js/components/store-list.js
@@ -18,21 +18,21 @@
         // I dont understand this part
         constructor(props) {
             this.stores = props.stores;
-            //let lastStores = this.LastStores;
+            let lastStores = this.LastStores;
         }
 
         update(props) {
             let fruits = props.fruit;
-            // let lastStores = this.lastStores;
+            let lastStores = this.lastStores;
 
-            // for(let i=0; i < stores.length; i++) {
-            //     let store = stores[i];
-            //     if(lastStore.includes(store)) continue;
-            //     this.renderStore(store);
-            // }
+            for(let i=0; i < stores.length; i++) {
+                let store = stores[i];
+                if(lastStore.includes(store)) continue;
+                this.renderStore(store);
+            }
 
-            // this.updateCount(this.stores.length);
-            // this.lastStores = stores.slice();
+            this.updateCount(this.stores.length);
+            this.lastStores = stores.slice();
         }
 
         renderStore(store) {
@@ -42,23 +42,23 @@
             this.table.appendChild(storeCard.render());
         }
 
-        // updateCount(count) {
-        //     this.countSpan.textContent = count;
-        // }
+        updateCount(count) {
+            this.countSpan.textContent = count;
+        }
 
         render() {
             let stores = this.stores;
             let dom = template();
             this.table = dom.querySelector('table');
-            // this.countSpan = dom.querySelector('store-count');
+            this.countSpan = dom.querySelector('store-count');
 
-            // for(let i = 0; i < fruits.length; i++) {
-            //     this.renderStore(stores[i]);
-            // }
+            for(let i = 0; i < fruits.length; i++) {
+                this.renderStore(stores[i]);
+            }
 
-            // this.updateCount(stores.length);
+            this.updateCount(stores.length);
 
-            // return dom;
+            return dom;
         }
     }
 
---
+++ js/store.api.js
@@ -0,0 +1,78 @@
+'use strict';
+
+(function(module) {
+
+    let stores;
+
+    let json = window.localStorage.getItem('stores');
+
+    if(json && json !== 'undefined') {
+        stores = JSON.parse(json);
+    }
+    else {
+        createStores();
+    }
+
+    function createStores() {
+        stores = [{
+            name: 'Pike Place Market',
+            key: 'pike',
+            minCust: 23,
+            maxCust: 65,
+            avgCookies: 6
+        }, {
+            name: 'SeaTac Airport',
+            key: 'seatac',
+            minCust: 3,
+            maxCust: 24,
+            avgCookies: 1
+        }, {
+            name: 'Seattle Center',
+            key: 'seattlecenter',
+            minCust: 11,
+            maxCust: 38,
+            avgCookies: 4
+        }, {
+            name: 'Capitol Hill',
+            key: 'caphill',
+            minCust: 20,
+            maxCust: 38,
+            avgCookies: 2
+        }, {
+            name: 'Alki',
+            key: 'alki',
+            minCust: 2,
+            maxCust: 16,
+            avgCookies: 5
+        }];
+
+        for(let i = 0; i < stores.length; i++) {
+            addKey(stores[i]);
+        } 
+    }
+
+    window.resetStores = createStores;
+
+    window.addEventListener('beforeunload', () => {
+        window.localStorage.setItem('stores', JSON.stringify(stores));
+    });
+
+
+    function addKey(store) {
+        store.key = store.name.split('').reverse().join();
+    }
+
+    let storeApi = {
+        get: function () {
+            return stores;
+        },
+        add: function(store) {
+            //dunno
+            }
+            addKey(store);
+            stores.push(store);
+    };
+
+    module.storeApi = storeApi;
+    
+})(window.module = window.module || {});
