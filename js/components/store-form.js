62a13e71adf857b15b69890a07fa6ba4b2f44f4c
Jack <ajackofsometrades@gmail.com>
2018-08-03 12:51:12 -0700

    set up imports/exports, class, and template for store-form

--- js/components/store-form.js
+++ js/components/store-form.js
@@ -2,6 +2,22 @@
 
 (function(module) {
 
+    let html = module.html;
 
+    let template = function() {
+        return html`
+            //some html
+        
+        `;
+    };
+
+    class StoreForm {
+
+
+    } 
+
+
+
+    module.StoreForm = StoreForm;
 
 })(window.module = window.module || {});
