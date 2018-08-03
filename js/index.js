7b85802cf088257119b54400fe41190ef38e31dd
Jack <ajackofsometrades@gmail.com>
2018-08-03 12:36:02 -0700

    logical phrasing

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@
 (function(module) {
 
     let App = module.App;
+    let app = new App();
+
     let root = document.getElementById('root');
 
-    let app = new App();
     let dom = app.render();
-
     root.appendChild(dom);
 
 })(window.module = window.module || {});
