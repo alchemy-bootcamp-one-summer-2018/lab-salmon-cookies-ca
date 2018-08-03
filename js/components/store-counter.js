8bf56ab026bd50ad2664ceb3a5142e66a188f91f
Jack <ajackofsometrades@gmail.com>
2018-08-03 16:10:35 -0700

    set up store counter

--- js/components/store-counter.js
+++ js/components/store-counter.js
@@ -1,7 +1,37 @@
 'use strict';
 
 (function(module) {
+    
+    let html = module.html;
+    
+    // let countSpan = document.getElementById('store-count');
+    // countSpan.textContent = storeAPI.read().length;
 
 
+    let template = function() {
+        return html`
+            <section>
+                <h3>
+                    Chose from one of over
+                    <span id="store-count"></span>
+                    amazing stores!
+                </h3>
+            </section>
+        `;
+    };
+
+    
+    class StoreCounter {
+
+        render() {
+            let dom = template();
+
+            return dom;
+        }
+
+
+    }
+
+    module.StoreCounter = StoreCounter;
 
 })(window.module = window.module || {});
