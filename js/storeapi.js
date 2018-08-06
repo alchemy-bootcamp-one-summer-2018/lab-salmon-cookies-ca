0806a97ca0b31bbc71ffb77dc47884af0fa6b41c
Alex Rankin <alex.c.rankin@gmail.com>
2018-08-05 22:06:29 -0700

    end of 8/5

--- js/storeapi.js
+++ js/storeapi.js
@@ -7,49 +7,38 @@
         minCustomers: 10,
         maxCustomers: 40,
         avgSale: 5,
+        hours: []
     }, {
         name: 'SeaTac Airport',
         minCustomers: 7,
         maxCustomers: 53,
         avgSale: 7,
+        hours: []
     }, {
         name: 'Seattle Center',
         minCustomers: 8,
         maxCustomers: 25,
         avgSale: 4,
+        hours: []
     }, {
         name: 'Capitol Hill',
         minCustomers: 12,
         maxCustomers: 44,
         avgSale: 6,
+        hours: []
     }, {
         name: 'Alki',
         minCustomers: 14,
         maxCustomers: 39,
         avgSale: 8,
+        hours: []
     }];
     
-    function customerCount(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
     
-    
-    
-    function getHourlySales(store){
-        let customers = 0;
-        let cookies = 0;
-        for(let j = 0; j < 14; j++){
-            customers = customerCount(store.minCustomers, store.maxCustomers);
-            cookies = customers * store.avgSale;
-            // store['hours'].push(Math.round(cookies));
-            console.log(customers, cookies)
-        }
-    }
 
     for(let i = 0; i < stores.length; i++){
         stores[i] = getHourlySales(stores[i]);
+        console.log(stores[i]);
     }
 
     
@@ -63,10 +52,11 @@
         }
     };
 
-    console.log(stores[2]);
+    console.log(stores);
 
     module.storeApi = storeApi;
 })(window.module = window.module || {});
+
 
 
 function customerCount(min, max) {
@@ -74,15 +64,15 @@ function customerCount(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-
-
+    
 function getHourlySales(store){
     let customers = 0;
     let cookies = 0;
     for(let j = 0; j < 14; j++){
         customers = customerCount(store.minCustomers, store.maxCustomers);
         cookies = customers * store.avgSale;
-        store['hours'].push(Math.round(cookies));
+        store.hours.push(cookies);
+        console.log(store.name, customers, cookies);
     }
 }
+
