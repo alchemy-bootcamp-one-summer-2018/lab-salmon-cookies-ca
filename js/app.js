eeb73f77626949086aefb42796cec35ed4f8b492
Jack <ajackofsometrades@gmail.com>
2018-08-06 06:34:21 -0700

    store form working

--- js/app.js
+++ js/app.js
@@ -11,26 +11,69 @@
 
     let template = function() {
         return html`        
-            <header>
-                <h2>Our Partners</h2>
+            <header class="header-container">
+                <a class="logo-container" href="/index.html" title="Salmon Industries - Home">
+                    <img src="/images/salmon.png" width="180">
+                    <h1 class="head-title">Salmon Industries</h1>
+                </a>
+                <nav>
+                
+                
+                    <ul>
+                        <li>
+                            <a class="active" href="/index.html" title="Home">Home</a>
+                        </li>
+                        <li>
+                            <a href="/pages/sales.html" title="Sales">Sales</a>
+                        </li>
+                        <li>
+                            <a href="/pages/products.html" title="Product Gallery">Product Gallery</a>
+                        </li>
+                        <li>
+                            <a href="/pages/about.html" title="Our story">Our Story</a>
+                        </li>
+                        <li>
+                            <a href="/pages/contact.html" title="Contact">Contact</a>
+                        </li>
+                    </ul>
+                
+                
+                </nav>
+            
+                <p class="core-info">
+                    <br> 12345 Main St
+                    <br> Portland, OR 97220
+                    <br> 12:02am - 11:58pm
+                    <br> (877) read - FISH
+                    <br>
+                    <a style="color: rgba(239, 132, 105, 1);" href="https://www.youtube.com/watch?v=i9SSOWORzw4">somethingisfishy@aol.com</a>
+                </p>
             </header>
             
-            <main></main>
+                
+            
+            
+            <main>
+                <h2 style="font-size: 46px;">Our Partners</h2>
+            </main>
         `;
     };
-
+    
     class App {
         render() {
             let dom = template();
             let main = dom.querySelector('main');
-
             let stores = storeAPI.read();
-
-    
-
-
-
-            let storeForm = new StoreForm;
+            
+            
+            let storeForm = new StoreForm({
+                onAdd: (store) => {
+                    storeAPI.create(store);
+                    storeTable.update({
+                        stores: stores
+                    });
+                }
+            });
             let storeCounter = new StoreCounter(
                 {
                     count: storeAPI.read().length,
@@ -45,12 +88,12 @@
             main.appendChild(storeForm.render());
             main.appendChild(storeCounter.render());
             main.appendChild(storeTable.render());
-
+            
             return dom;
 
         }
     }
-
+    
     module.App = App;
 
-})(window.module = window.module || {});
+}(window.module = window.module || {}));
